c9a34eab5b5742c1989a1da84f1850a1e16ee1e7
tiagokamegasawa <tiago.kamegasawa93@gmail.com>
2020-04-09 01:36:53 +0000

    Trocando path do s3

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-1dvp-12345"
+    bucket = "hackathon-fiap-34scj-333483"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
